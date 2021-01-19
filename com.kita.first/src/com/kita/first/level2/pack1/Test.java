0978ba21fb1d6b99bbd6d32e5500ede420ca930d
wol6425737 <wol6425737@naver.com>
2021-01-19 15:49:42 +0900

    0119

--- com.kita.first/src/com/kita/first/level2/pack1/Test.java
+++ com.kita.first/src/com/kita/first/level2/pack1/Test.java
@@ -1,27 +1,34 @@
 package com.kita.first.level2.pack1;
 
+//import java.util.Scanner;//Ctrl Shift O
+
 public class Test {
 	public static void main(String[] args) {
-		int n1 = 3;
-		int n2 = 5;
-		int n3 = 7;
-		FinalStatic fs1 = new FinalStatic(n1,n2);
-		FinalStatic fs2 = new FinalStatic(n2,n3);
-		FinalStatic fs3 = new FinalStatic(n3,n1);
+//		int n1 = 3;
+//		int n2 = 5;
+//		int n3 = 7;
+//		FinalStatic fs1 = new FinalStatic(n1,n2);
+//		FinalStatic fs2 = new FinalStatic(n2,n3);
+//		FinalStatic fs3 = new FinalStatic(n3,n1);
+//		
+//		System.out.println(fs1.N1);
+//		System.out.println(fs2.N1);
+//		System.out.println(fs3.N1);
+//		//final int N1;으로 해서 상수로 선언했으나 다 결과값이 다르게 나옴
+//		System.out.println(fs1.N2);
+//		System.out.println(fs2.N2);
+//		System.out.println(fs3.N2);
+//		//static final int N2로 해야 상수가 됨.
+//		
+//		Singleton singleton = Singleton.getInstance();
+//		
+//		FinalStatic.printNum(1);
+//
+//		Scanner sc = new Scanner(System.in);
 		
-		System.out.println(fs1.N1);
-		System.out.println(fs2.N1);
-		System.out.println(fs3.N1);
-		//final int N1;으로 해서 상수로 선언했으나 다 결과값이 다르게 나옴
-		System.out.println(fs1.N2);
-		System.out.println(fs2.N2);
-		System.out.println(fs3.N2);
-		//static final int N2로 해야 상수가 됨.
-		
-		Singleton singleton = Singleton.getInstance();
-		
-		FinalStatic.printNum(1);
-
+		Class1 c1 = new Class1(1, 2);
+		System.out.println(c1.n1);
+//		com.kita.first.level2.pack2.Class1 c2 = new com.kita.first.level2.pack2.Class1();
 	}
 
 }
--- com.kita.first/src/com/kita/first/level2/pack2/Test.java
+++ com.kita.first/src/com/kita/first/level2/pack2/Test.java
@@ -1,5 +1,9 @@
 package com.kita.first.level2.pack2;
 
-public class Test {
+public class Test extends com.kita.first.level2.pack2.Class1 {
+	public static void main(String[] args) {
+//		Class1 c1 = new Class1();
+		com.kita.first.level2.pack1.Class1 c2 = new com.kita.first.level2.pack1.Class1(1);
+	}
 
 }
