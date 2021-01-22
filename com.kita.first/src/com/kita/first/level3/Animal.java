5ae547bf7b0a97bba35a410a58826ccd3915bcde
wol6425737 <wol6425737@naver.com>
2021-01-22 15:47:34 +0900

    0122

---
+++ com.kita.first/src/com/kita/first/level3/Animal.java
@@ -0,0 +1,8 @@
+package com.kita.first.level3;
+
+public class Animal {
+	void crying() {
+		System.out.println("동물이 운다.");
+	}
+
+}
---
+++ com.kita.first/src/com/kita/first/level3/ProGamer.java
@@ -0,0 +1,8 @@
+package com.kita.first.level3;
+
+public class ProGamer {
+	void playGame(Game game) {
+		game.play();
+	}
+
+}
