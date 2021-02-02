8bd80a358df417f4fc8b2e61a6000f4c8e88825c
wol6425737 <wol6425737@naver.com>
2021-02-02 15:47:38 +0900

    0202

--- com.kita.first/src/com/kita/first/level4/Cd.java
+++ com.kita.first/src/com/kita/first/level4/Cd.java
@@ -1,5 +1,14 @@
 package com.kita.first.level4;
 
 public class Cd {
+	String name;
+	String producer;
+	int year;
+	
 
+	public Cd(String name, String producer, int year) {
+		this.name = name;
+		this.producer = producer;
+		this.year = year;
+	}
 }
--- com.kita.first/src/com/kita/first/level4/ListCollection.java
+++ com.kita.first/src/com/kita/first/level4/ListCollection.java
@@ -1,6 +1,7 @@
 package com.kita.first.level4;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ListCollection {
@@ -17,6 +18,19 @@ public class ListCollection {
 		list.remove(2);
 		String result = list.get(1);
 		System.out.println(result);
+		
+		//리스트에 삽입과 삭제가 빈번할 경우에는 LinkedList가 더 빠르다.자료를 뽑아올 때에는 느리다.
+		//List<> list = new LinkedList<>();
+		
+		List<Cd> shelf = new LinkedList<Cd>();
+		Cd cd1 = new Cd("여름","아이유",2019);
+		Cd cd2 = new Cd("항해","악동뮤지션",2020);
+		
+		shelf.add(cd1);
+		shelf.add(cd2);
+		shelf.add(cd2); //중복으로 넣을 수 있음, LinkedList는 객체를 넣는게 아니라 객체 주소를 넣는 것
+		
+		
 		
 		
 		
